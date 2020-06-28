dbe6128be9060de70109df275e4743bf10858d8e
Joshua K Bader <jaybshinsen@hotmail.com>
2020-06-29 00:38:25 +0800

    added chalk and a mongoose connect check.

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
+router.post("/", (req, res) => {
+  
+})
+
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
--- server.js
+++ server.js
@@ -1,28 +1,35 @@
+//NPM Packages
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
-const PORT = 3000;
-
 const app = express();
+const chalk = require('chalk');
 
+//MiddleWare
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+// Mongoose
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
-// routes
+// Ports and Routes
+const PORT = process.env.PORT || 8080;
 app.use(require("./routes/api.js"));
 
+// Listener
+mongoose.connection
+  .once("open", () => console.log(chalk.magentaBright("Connected to Mongoose")))
+  .on("error", (error) => {
+    console.log(chalk.red("Your Error: ", error));
+  });
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(chalk.blue.inverse(`App running on port ${PORT}!`));
 });
